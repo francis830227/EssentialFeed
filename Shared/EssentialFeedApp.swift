9570fe091f54a89371f7d2f5c5cb8c5e3ba73719
Tseng Kai Ping <francis830227@gmail.com>
2022-04-14 15:55:57 +0800

    unit tests add to project

---
+++ Shared/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  Shared
+//
+//  Created by Kai-Ping Tseng on 2022/4/14.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
