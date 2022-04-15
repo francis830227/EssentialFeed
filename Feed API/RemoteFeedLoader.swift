3ed173634506587b89006ba5f5928e96c0c32d22
Tseng Kai Ping <francis830227@gmail.com>
2022-04-16 00:12:54 +0800

    Delivers invalidData error on 200 HTTP response with invalid json

--- Feed API/RemoteFeedLoader.swift
+++ Feed API/RemoteFeedLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum HTTPClientResult {
-    case success(HTTPURLResponse)
+    case success(Data, HTTPURLResponse)
     case failure(Error)
 }
 
@@ -32,7 +32,6 @@ public final class RemoteFeedLoader {
     
     public func load(completion: @escaping (Error) -> Void) {
         client.get(from: url) { result in
-            
             switch result {
             case .success:
                 completion(.invalidData)
