95af920b24ec817987e467c5f1b05a0bbe7703ad
Tseng Kai Ping <francis830227@gmail.com>
2022-06-16 16:16:32 +0800

    Load command delivers cached images on less than seven days old cache

--- Feed Cache/LocalFeedLoader.swift
+++ Feed Cache/LocalFeedLoader.swift
@@ -31,10 +31,13 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retreive { error in
-            if let error = error {
+        store.retreive { result in
+            switch result {
+            case let .failure(error):
                 completion(.failure(error))
-            } else {
+            case let .found(feed, _):
+                completion(.success(feed.toModels()))
+            case .empty:
                 completion(.success([]))
             }
         }
@@ -51,5 +54,13 @@ public final class LocalFeedLoader {
 private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
+    
+    
+}
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
 }
